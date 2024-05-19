d38c70c818bee10fbd76f5230aaec8658c5312a7
marvenmalope <marvenmalope2003@gmail.com>
2024-05-19 16:55:27 +0200

    db

--- k/src/main/java/com/swp/k/persistence/entity/History.java
+++ k/src/main/java/com/swp/k/persistence/entity/History.java
@@ -1,10 +1,16 @@
 package com.swp.k.persistence.entity;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
@@ -12,6 +18,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class History {
@@ -19,7 +26,11 @@ public class History {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer history_id;
-    private Integer pulse_rate;
+
+    @CollectionTable(name = "pulse_rate_history", joinColumns = { @JoinColumn(name = "history_id")} )
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<Integer> pulse_rate;
+
     private String mood;
 
     @Temporal(value = TemporalType.DATE)
--- k/src/main/java/com/swp/k/persistence/entity/Member.java
+++ k/src/main/java/com/swp/k/persistence/entity/Member.java
@@ -5,14 +5,11 @@ import java.util.Date;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
