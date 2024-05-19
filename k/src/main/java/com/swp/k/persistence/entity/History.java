773e9fce44499fe2b46e4986f98bc74363c2193a
marvenmalope <marvenmalope2003@gmail.com>
2024-05-19 15:37:26 +0200

    dsda

--- k/src/main/java/com/swp/k/persistence/entity/History.java
+++ k/src/main/java/com/swp/k/persistence/entity/History.java
@@ -1,5 +1,12 @@
 package com.swp.k.persistence.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +15,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer history_id;
     private Integer pulse_rate;
-    private 
     private String mood;
+
+    @Temporal(value = TemporalType.DATE)
+    private Date date_added;
 }
