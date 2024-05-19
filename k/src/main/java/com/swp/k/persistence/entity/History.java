7ab6d7d8edc4f36e91808d5745ca16332e468f87
marvenmalope <marvenmalope2003@gmail.com>
2024-05-19 15:34:51 +0200

    fdffd

---
+++ k/src/main/java/com/swp/k/persistence/entity/History.java
@@ -0,0 +1,15 @@
+package com.swp.k.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+    private Integer history_id;
+    private Integer pulse_rate;
+    private 
+    private String mood;
+}
